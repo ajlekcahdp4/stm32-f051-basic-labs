9f76111d3547c0d091127a50ed9f70913459d713
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-04-26 13:02:41 +0300

    rainbow effect

--- final-project/src/main.cc
+++ final-project/src/main.cc
@@ -4,6 +4,7 @@
 #include "stm32f051/nvic.hpp"
 #include "stm32f051/rcc.hpp"
 #include "stm32f051/tim2.hpp"
+#include "stm32f051/tim3.hpp"
 #include <stm32f051x8.h>
 
 using namespace stmcpp::stm32f051;
@@ -16,7 +17,11 @@ enum class modes {
   ALL,
   RED,
   GREEN,
-  BLUE
+  BLUE,
+  PURPLE,
+  BLINK_ALL,
+  RAINBOW,
+  COUNT
 };
 
 int index(const modes volatile &md) {
@@ -24,12 +29,7 @@ int index(const modes volatile &md) {
 }
 
 modes index_mode(int idx) {
-  switch (idx % 4) {
-  case 0: return modes::ALL;
-  case 1: return modes::RED;
-  case 2: return modes::GREEN;
-  case 3: return modes::BLUE;
-  }
+  return static_cast<modes>(idx % static_cast<int>(modes::COUNT));
 }
 
 modes volatile mode = modes::ALL;
@@ -38,8 +38,8 @@ void board_clocking_init() {
   mrcc::cr |= mrcc::cr_fields::hseon;
 }
 
+// magnetic switch and button input
 void tim2_config(unsigned dur) {
-  NVIC_SetPriority(TIM2_IRQn, 1);
   mnvic::iser_register{}.set(mtim2::nvic_bit);
   mrcc::apb1enr |= mrcc::apb1enr_fields::tim2en;
   mtim2::arr = dur;
@@ -84,30 +84,27 @@ void gpioc_init() {
 constexpr auto blue = mgpiob::odr_fields::odr1.mask;
 constexpr auto red = mgpiob::odr_fields::odr2.mask;
 constexpr auto green = mgpiob::odr_fields::odr10.mask;
+constexpr auto purple = mgpiob::odr_fields::odr1.mask | mgpiob::odr_fields::odr2.mask;
 constexpr auto led_pins = blue | red | green;
 
-void enable_all_leds() {
+void enable_color(decltype(mgpiob::odr_fields::odr1.mask) color) {
   auto suround_state = ~led_pins & mgpiob::odr_register{}.get();
-  auto to_write = led_pins;
-  mgpiob::odr_register{}.set(to_write | suround_state);
+  mgpiob::odr_register{}.set(color | suround_state);
 }
 
-void enable_blue_leds() {
-  auto suround_state = ~led_pins & mgpiob::odr_register{}.get();
-  auto to_write = blue;
-  mgpiob::odr_register{}.set(to_write | suround_state);
+void delay_ms(uint32_t time) {
+  for (uint32_t i = 0; i < time * tacts_per_one_us / 1000; ++i) {
+    // Insert NOP for power consumption:
+    __asm__ volatile("nop");
+  }
 }
 
-void enable_red_leds() {
+void blink_leds(decltype(mgpiob::odr_fields::odr1.mask) color) {
   auto suround_state = ~led_pins & mgpiob::odr_register{}.get();
-  auto to_write = red;
-  mgpiob::odr_register{}.set(to_write | suround_state);
-}
-
-void enable_green_leds() {
-  auto suround_state = ~led_pins & mgpiob::odr_register{}.get();
-  auto to_write = green;
-  mgpiob::odr_register{}.set(to_write | suround_state);
+  mgpiob::odr_register{}.set(suround_state);
+  delay_ms(7000);
+  mgpiob::odr_register{}.set(color | suround_state);
+  delay_ms(7000);
 }
 
 void disable_all_leds() {
@@ -115,31 +112,65 @@ void disable_all_leds() {
   mgpiob::odr_register{}.set(suround_state);
 }
 
-void delay_ms(uint32_t time) {
-  for (uint32_t i = 0; i < time * tacts_per_one_us; ++i) {
-    // Insert NOP for power consumption:
-    __asm__ volatile("nop");
+void rainbow() {
+  static int counter = 0;
+  const int max_cnt = 3000;
+  const int third_cnt = max_cnt / 3;
+
+  if (counter <= third_cnt) {
+    enable_color(red);
+    delay_ms(third_cnt - counter);
+    disable_all_leds();
+    enable_color(green);
+    delay_ms(counter);
+    disable_all_leds();
+  } else if (counter <= 2 * third_cnt) {
+    enable_color(green);
+    delay_ms(2 * third_cnt - counter);
+    disable_all_leds();
+    enable_color(blue);
+    delay_ms(counter - third_cnt);
+    disable_all_leds();
+  } else {
+    enable_color(blue);
+    delay_ms(max_cnt - counter);
+    disable_all_leds();
+    enable_color(red);
+    delay_ms(counter - 2 * third_cnt);
+    disable_all_leds();
   }
+
+  counter = (counter + 1) % max_cnt;
 }
 
 void output() {
   switch (mode) {
-  case modes::ALL: {
-    enable_all_leds();
-    return;
+  case modes::ALL: enable_color(led_pins); return;
+  case modes::RED: enable_color(red); return;
+  case modes::BLUE: enable_color(blue); return;
+  case modes::GREEN: enable_color(green); return;
+  case modes::PURPLE: enable_color(purple); return;
+  case modes::BLINK_ALL: blink_leds(led_pins); return;
+  case modes::RAINBOW: rainbow(); return;
   }
-  case modes::RED: {
-    enable_red_leds();
-    return;
-  }
-  case modes::BLUE: {
-    enable_blue_leds();
-    return;
-  }
-  case modes::GREEN: {
-    enable_green_leds();
-    return;
-  }
+}
+
+// PWM of leds
+void tim3_config(unsigned dur) {
+  NVIC_SetPriority(TIM3_IRQn, 2);
+  mnvic::iser_register{}.set(mtim3::nvic_bit);
+  mrcc::apb1enr |= mrcc::apb1enr_fields::tim3en;
+  mtim3::arr = dur;
+  mtim3::dier |= mtim3::dier_fields::uie_enabled;
+  mtim3::cr1 |= mtim3::cr1_fields::cen_enabled;
+}
+
+extern "C" void TIM3_IRQHandler() {
+  mtim3::sr &= mtim3::sr_fields::uif_clear;
+  if (enabled) {
+    output();
+  } else {
+    disable_all_leds();
   }
 }
 
@@ -149,10 +180,8 @@ int main() {
   gpiob_init();
   gpioc_init();
   tim2_config(SystemCoreClock / 1000 * 5);
+  tim3_config(SystemCoreClock / 100);
 
-  for (;;) {
-    if (enabled) output();
-    else disable_all_leds();
-    delay_ms(50);
-  }
+  for (;;)
+    ;
 }
